23113bd8945f7b2a380cde0a58ae375741ce405b
boudoual <alexandre.bourdouxhe@arhs-dev-be.com>
2021-02-23 08:22:13 +0100

    Creating jokes app project and adding it to git

---
+++ src/test/java/spring/training/personal/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.training.personal.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
